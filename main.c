61973369144af1751851e1a093385c479dde14eb
Nuno Alves <nalves@me.com>
2012-03-17 03:55:13 -0400

    minor tabulation changes...

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 /** 
-* @file main.c
-*
-* @brief Addressing multiple BeagleBone GPIO pins in C
-* @details Self-contined program that will turn ON/OFF a specified group of
-* of GPIO pins in the beaglebone board. This code was possible due to prior 
-* efforts by Christian Mitchell, Markus Klink and Matt Richardson. To compile
-* and run this code, type something like "gcc main.c ; ./a.out ;  rm -f a.out"
-*
-* @author Nuno Alves
-* @date 17/March/2012 
-**/
+ * @file main.c
+ *
+ * @brief Addressing multiple BeagleBone GPIO pins in C
+ * @details Self-contined program that will turn ON/OFF a specified group of
+ * of GPIO pins in the beaglebone board. This code was possible due to prior 
+ * efforts by Christian Mitchell, Markus Klink and Matt Richardson. To compile
+ * and run this code, type something like "gcc main.c ; ./a.out ;  rm -f a.out"
+ *
+ * @author Nuno Alves
+ * @date 17/March/2012 
+ **/
 
 #include <stdio.h> 
 #include <stdlib.h> 
@@ -30,7 +30,7 @@
 #define P8_12 4
 
 /** @brief Contains all elements which we need to pass into the beagleboard 
-* to specify which GPIO pin we want to turn ON/OFF **/ 
+ * to specify which GPIO pin we want to turn ON/OFF **/ 
 struct gpioID
 {
 	char *PINNAME;  //eg. P8_3
@@ -40,15 +40,15 @@ struct gpioID
 };
 
 /** 
-* @brief Writes a value to a particular GPIO, logic-HIGH or logic-LOW.
-* @param GPIONUMBER The GPIO number (e.g. 38 for gpio1[6])
-* @param value The logic value we want to write to the GPIO (0 or 1)
-**/ 
-        void write_GPIO_value(int GPIONUMBER, int value)
+ * @brief Writes a value to a particular GPIO, logic-HIGH or logic-LOW.
+ * @param GPIONUMBER The GPIO number (e.g. 38 for gpio1[6])
+ * @param value The logic value we want to write to the GPIO (0 or 1)
+ **/ 
+void write_GPIO_value(int GPIONUMBER, int value)
 {
 	char export_filename[50]; 
 	FILE *f = NULL; 
-	 	 
+    
 	sprintf(export_filename, "/sys/class/gpio/gpio%d/value",GPIONUMBER); 
 	f = fopen(export_filename,"w");
 	assert(f!=NULL);    
@@ -57,11 +57,11 @@ struct gpioID
 }
 
 /** 
-* @brief Tells the OS that we are done with using GPIOs
-* @param selected_GPIOs[] initialized array of gpioID.
-* @param selectedPins[] Array with the user specified pins
-* @param sizeof_selectedPins Number of pins that were specified by the user
-**/ 
+ * @brief Tells the OS that we are done with using GPIOs
+ * @param selected_GPIOs[] initialized array of gpioID.
+ * @param selectedPins[] Array with the user specified pins
+ * @param sizeof_selectedPins Number of pins that were specified by the user
+ **/ 
 void cleanup_GPIO(struct gpioID selected_GPIOs[],int selectedPins[], int nbr_selectedPins)
 {
 	char export_filename[50]; 
@@ -80,20 +80,20 @@ void cleanup_GPIO(struct gpioID selected_GPIOs[],int selectedPins[], int nbr_sel
 }
 
 /** 
-* @brief Initializes all the user specified pins  
-* @param selected_GPIOs[] Un-initialized array of gpioID. At the end of this 
-* function, the selected_GPIOs[] array will be initialized.
-* @param selectedPins[] Array with the user specified pins
-* @param sizeof_selectedPins Number of pins that were specified by the user
-**/ 
+ * @brief Initializes all the user specified pins  
+ * @param selected_GPIOs[] Un-initialized array of gpioID. At the end of this 
+ * function, the selected_GPIOs[] array will be initialized.
+ * @param selectedPins[] Array with the user specified pins
+ * @param sizeof_selectedPins Number of pins that were specified by the user
+ **/ 
 void initialize_each_enabled_gpio(struct gpioID selected_GPIOs[],int selectedPins[], int nbr_selectedPins)
 {
 	int i;
 	//this variable will contain the location of the file that we need to 
 	//set to mode 7
 	char export_filename[50]; 
-
-	 FILE *f = NULL; 
+    
+    FILE *f = NULL; 
 	//You are not allowed to use more than 32 active pins. This is not a 
 	//beagleboard limitation, but a limitation of this code.
 	assert (nbr_selectedPins<=32);
@@ -110,35 +110,35 @@ void initialize_each_enabled_gpio(struct gpioID selected_GPIOs[],int selectedPin
 				selected_GPIOs[i].GPIONUMBER=38;
 				selected_GPIOs[i].GPIOMUX="gpmc_ad6";
 				break;
-
+                
 			case P8_4:
 				selected_GPIOs[i].PINNAME="P8_4";
 				selected_GPIOs[i].GPIOID="gpio1[7]";
 				selected_GPIOs[i].GPIONUMBER=39;
 				selected_GPIOs[i].GPIOMUX="gpmc_ad7";
 				break;
-
+                
 			case P8_5:
 				selected_GPIOs[i].PINNAME="P8_5";
 				selected_GPIOs[i].GPIOID="gpio1[2]";
 				selected_GPIOs[i].GPIONUMBER=34;
 				selected_GPIOs[i].GPIOMUX="gpmc_ad2";
 				break;
-
+                
 			case P8_11:
 				selected_GPIOs[i].PINNAME="P8_11";
 				selected_GPIOs[i].GPIOID="gpio1[13]";
 				selected_GPIOs[i].GPIONUMBER=45;
 				selected_GPIOs[i].GPIOMUX="gpmc_ad13";
 				break;
-
+                
 			case P8_12:
 				selected_GPIOs[i].PINNAME="P8_12";
 				selected_GPIOs[i].GPIOID="gpio1[12]";
 				selected_GPIOs[i].GPIONUMBER=44;
 				selected_GPIOs[i].GPIOMUX="gpmc_ad12";
 				break;
-
+                
 			default:
 				//This statement should never be reached... since
 				//the program won't even compile if the user 
@@ -153,25 +153,25 @@ void initialize_each_enabled_gpio(struct gpioID selected_GPIOs[],int selectedPin
 		//set mux to mode 7 
 	 	sprintf(export_filename, "/sys/kernel/debug/omap_mux/%s", selected_GPIOs[i].GPIOMUX); 
 		f = fopen(export_filename,"w");
-		 
+        
 		if (f == NULL)
 		{
          	printf( "\nERROR: There was a problem opening /sys/kernel/debug/omap_mux/%s\n", selected_GPIOs[i].GPIOMUX); 
 			printf("\n%s\t%s\t%s\t%d\n\n", selected_GPIOs[i].PINNAME, selected_GPIOs[i].GPIOID, selected_GPIOs[i].GPIOMUX, selected_GPIOs[i].GPIONUMBER);
-
+            
 			assert(f!=NULL);
         } 
         fprintf(f, "7"); 
         pclose(f); 
-
+        
 		//export the pin
 		f = fopen("/sys/class/gpio/export","w");
-		 
+        
 		if (f == NULL)
 		{
          	printf( "\nERROR: There was a problem opening /sys/kernel/debug/omap_mux/%s\n", selected_GPIOs[i].GPIOMUX); 
 			printf("\n%s\t%s\t%s\t%d\n\n", selected_GPIOs[i].PINNAME,selected_GPIOs[i].GPIOID,selected_GPIOs[i].GPIOMUX,selected_GPIOs[i].GPIONUMBER);
-
+            
 			assert(f!=NULL);
         } 
         fprintf(f, "%d",selected_GPIOs[i].GPIONUMBER); 
@@ -180,12 +180,12 @@ void initialize_each_enabled_gpio(struct gpioID selected_GPIOs[],int selectedPin
         //set the appropriate io direction (out)
 	 	sprintf(export_filename, "/sys/class/gpio/gpio%d/direction", selected_GPIOs[i].GPIONUMBER); 
 		f = fopen(export_filename,"w");
-		 
+        
 		if (f == NULL)
 		{
          	printf( "\nERROR: There was a problem opening /sys/class/gpio/gpio%d/direction\n", selected_GPIOs[i].GPIONUMBER); 
 			printf("\n%s\t%s\t%s\t%d\n\n",selected_GPIOs[i].PINNAME,selected_GPIOs[i].GPIOID,selected_GPIOs[i].GPIOMUX,selected_GPIOs[i].GPIONUMBER);
-
+            
 			assert(f!=NULL);
         } 
         fprintf(f , "out" ); 
@@ -197,18 +197,18 @@ void initialize_each_enabled_gpio(struct gpioID selected_GPIOs[],int selectedPin
 
 
 /** 
-* @brief Displays all information for each of the user specified pins. 
-* This function has no purpose besides live debugging.
-* @param selected_GPIOs[] An valid (initialized) array of gpioID
-* @param nbr_selectedPins Number of pins that were specified by the user
-**/ 
+ * @brief Displays all information for each of the user specified pins. 
+ * This function has no purpose besides live debugging.
+ * @param selected_GPIOs[] An valid (initialized) array of gpioID
+ * @param nbr_selectedPins Number of pins that were specified by the user
+ **/ 
 void display_each_enabled_gpio(struct gpioID selected_GPIOs[],int nbr_selectedPins)
 {
 	int i;
 	printf("\n");
 	printf("PINNAME\tGPIOID\t\tGPIOMUX\t\tGPIONBR\n");
 	printf("++++++++++++++++++++++++++++++++++++++++++++++++\n");
-
+    
 	for (i=0; i< nbr_selectedPins; i++)
 	{
 		printf("%s\t%s\t%s\t%d\n",selected_GPIOs[i].PINNAME,selected_GPIOs[i].GPIOID,selected_GPIOs[i].GPIOMUX,selected_GPIOs[i].GPIONUMBER);
@@ -217,13 +217,13 @@ void display_each_enabled_gpio(struct gpioID selected_GPIOs[],int nbr_selectedPi
 }
 
 /** 
-* @brief Cycles through the entire array of selected_GPIOs[] and turns ON/OFF
-* each associated GPIO.
-* @param selected_GPIOs[] An valid (initialized) array of gpioID
-* @param data_to_write An unsigned integer (32 bits), where each bit contains the 
-* information regarding the status of each pin: 1=turn pin ON, 0=turn pin OFF
-* @param nbr_selectedPins Number of pins that were specified by the user
-**/ 
+ * @brief Cycles through the entire array of selected_GPIOs[] and turns ON/OFF
+ * each associated GPIO.
+ * @param selected_GPIOs[] An valid (initialized) array of gpioID
+ * @param data_to_write An unsigned integer (32 bits), where each bit contains the 
+ * information regarding the status of each pin: 1=turn pin ON, 0=turn pin OFF
+ * @param nbr_selectedPins Number of pins that were specified by the user
+ **/ 
 void turn_ON_OFF_pins(struct gpioID selected_GPIOs[],unsigned int data_to_write,int nbr_selectedPins)
 {
 	int i;
@@ -232,7 +232,7 @@ void turn_ON_OFF_pins(struct gpioID selected_GPIOs[],unsigned int data_to_write,
 	{
 		//code that turns ON/OFF a pin
 		write_GPIO_value(selected_GPIOs[i].GPIONUMBER,bitRead(data_to_write,i));
-
+        
 		//this is just for debugging purposes
 		if (DEBUG)
 		{ 
@@ -245,13 +245,13 @@ void turn_ON_OFF_pins(struct gpioID selected_GPIOs[],unsigned int data_to_write,
 }
 
 /** 
-* @brief Writes a bit into a particular location in an unsigned int variable (32 bits)
-* @param groupOfBits Some data resides in these 32 bits 
-* @param bitVal The bitVal is wither a 1 or a 0
-* @param bitLoc Where in the groupOfBits, the value of bitVal will be written. 
-* Keep in mind that the bitLoc values ranges 0 to 31
-* @return Returns the updated groupOfBits
-**/ 
+ * @brief Writes a bit into a particular location in an unsigned int variable (32 bits)
+ * @param groupOfBits Some data resides in these 32 bits 
+ * @param bitVal The bitVal is wither a 1 or a 0
+ * @param bitLoc Where in the groupOfBits, the value of bitVal will be written. 
+ * Keep in mind that the bitLoc values ranges 0 to 31
+ * @return Returns the updated groupOfBits
+ **/ 
 unsigned int bitWrite(unsigned int groupOfBits, int bitVal, int bitLoc)
 {
     if (bitVal==1) groupOfBits |= 1 << bitLoc;
@@ -260,29 +260,29 @@ unsigned int bitWrite(unsigned int groupOfBits, int bitVal, int bitLoc)
 }
 
 /** 
-* @brief Returns the bit value of a particular location in an unsigned int variable (32 bits)
-* @param groupOfBits Some data resides in these 32 bits 
-* @param bitLoc Where in the groupOfBits, the value of bitVal will be written 
-* @return Retuns an integer with the bit value (1 or 0) that resides in bitLoc
-**/ 
+ * @brief Returns the bit value of a particular location in an unsigned int variable (32 bits)
+ * @param groupOfBits Some data resides in these 32 bits 
+ * @param bitLoc Where in the groupOfBits, the value of bitVal will be written 
+ * @return Retuns an integer with the bit value (1 or 0) that resides in bitLoc
+ **/ 
 int bitRead(unsigned int groupOfBits, int bitLoc)
 {
     unsigned int bit = groupOfBits & (1 << bitLoc);
     if (bit!=0) bit=1;
-
+    
     return(bit);
 }
 
 /** 
-* @brief This is the main function with a sample test program.
-* @return Returns a 1 upon succesful program termination
-**/ 
+ * @brief This is the main function with a sample test program.
+ * @return Returns a 1 upon succesful program termination
+ **/ 
 int main()
 {
 	
 	int selectedPins[]={P8_3,P8_4,P8_5,P8_11,P8_12};
 	int nbr_selectedPins=sizeof(selectedPins)/sizeof(*selectedPins);
-
+    
 	//This variable data_to_write will contain the information which specified
 	//the pins that will be ON or OFF
 	unsigned int data_to_write=0;
@@ -290,7 +290,7 @@ int main()
 	//Define an array of gpios which will contain all the user selected pins and
 	//its associated information
 	struct gpioID enabled_gpio[nbr_selectedPins];
-
+    
 	//Initializes all user defined pins
 	initialize_each_enabled_gpio(enabled_gpio,selectedPins,nbr_selectedPins);
 	if (DEBUG) display_each_enabled_gpio(enabled_gpio,nbr_selectedPins);
@@ -308,29 +308,29 @@ int main()
 	//Which is equivalent to the value 10, in base 10.	
     data_to_write=10; 
 	turn_ON_OFF_pins(enabled_gpio,data_to_write,nbr_selectedPins);
-
-	 sleep(1); //sleep for 1 second
-	 
+    
+    sleep(1); //sleep for 1 second
+    
 	//In addition to the pins that are already ON (P8_4, P8_11), I now
 	//also want to turn ON pin P8_12. Since P8_12 is on position 4, I 
 	//need to write a 1 to position 4.
     data_to_write=bitWrite(data_to_write,1,4);
 	turn_ON_OFF_pins(enabled_gpio,data_to_write,nbr_selectedPins);
-
+    
     //Of course, I could have done something like
     //data_to_write=26; 
     //turn_ON_OFF_pins(enabled_gpio,data_to_write,nbr_selectedPins);
 	//Which would have yielded the exact same outcome.
- 
-	 sleep(1); //sleep for 1 second
-
+    
+    sleep(1); //sleep for 1 second
+    
     //Now I would like to turn OFF pin P8_11, while leaving pins P8_4 & P8_12 ON
 	//Since pin P8_11 is on position 3
-  	 data_to_write=bitWrite(data_to_write,0,3);
+    data_to_write=bitWrite(data_to_write,0,3);
 	turn_ON_OFF_pins(enabled_gpio,data_to_write,nbr_selectedPins);
 	
-	 sleep(1); //sleep for 1 second
-
+    sleep(1); //sleep for 1 second
+    
 	//we should now tell the OS that we are done with the GPIOs
 	cleanup_GPIO(enabled_gpio,selectedPins,nbr_selectedPins);
 }
